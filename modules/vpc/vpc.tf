dc8d2359801d76002ee7d54a70d5974902142bc4
radhikaepiq <103490335+radhikaepiq@users.noreply.github.com>
2023-03-07 13:47:27 +0530

    Update vpc.tf

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -1,14 +1,14 @@
-# resource "aws_vpc" "EKSVPC" {
-#   cidr_block       = "${var.vpc_cidr}"
-#   tags = {
-#     Name = "${var.Namevpc}"
-#     Purpose ="${var.Purpose}"
-#     Owner = "${var.Owner}"
-#   }
-# }
+resource "aws_vpc" "EKSVPC" {
+  cidr_block       = "${var.vpc_cidr}"
+  tags = {
+    Name = "${var.Namevpc}"
+    Purpose ="${var.Purpose}"
+    Owner = "${var.Owner}"
+  }
+}
 
-# ///output
+///output
 
-# output "vpc_id" {
-#   value = "${aws_vpc.EKSVPC.id}"
-# }
+output "vpc_id" {
+  value = "${aws_vpc.EKSVPC.id}"
+}
