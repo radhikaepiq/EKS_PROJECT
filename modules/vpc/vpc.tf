4e974b6e75cfeb7ab6a2d14852dab420fdd2b683
radhikaN <radhika@gmail.com>
2023-03-09 17:03:55 +0530

    vpc

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-=======
-# <<<<<<< HEAD
-resource "aws_vpc" "EKSVPC" {
-  cidr_block       = "${var.vpc_cidr}"
-  tags = {
-    Name = "${var.Namevpc}"
-    Purpose ="${var.Purpose}"
-    Owner = "${var.Owner}"
-  }
-}
-
-///output
-
-output "vpc_id" {
-  value = "${aws_vpc.EKSVPC.id}"
-}
->>>>>>> 2a76104a650b15ecaa1e95fa07a7852194cb7b60
 
 resource "aws_vpc" "EKSVPC" {
   cidr_block       = "${var.vpc_cidr}"
@@ -31,23 +13,3 @@ resource "aws_vpc" "EKSVPC" {
 output "vpc_id" {
   value = "${aws_vpc.EKSVPC.id}"
 }
-<<<<<<< HEAD
-# =======
-resource "aws_vpc" "EKSVPC" {
-  cidr_block       = "${var.vpc_cidr}"
-  tags = {
-    Name = "${var.Namevpc}"
-    Purpose ="${var.Purpose}"
-    Owner = "${var.Owner}"
-  }
-}
-
-# # ///output
-
-output "vpc_id" {
-  value = "${aws_vpc.EKSVPC.id}"
-}
-
-=======
-# >>>>>>> d0470fa (done)
->>>>>>> 2a76104a650b15ecaa1e95fa07a7852194cb7b60
