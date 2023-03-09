b252fafabd63daed699509cc0114a01c8e1580af
radhikaN <radhika@gmail.com>
2023-03-09 14:14:02 +0530

    last updated

--- main.tf
+++ main.tf
@@ -31,33 +31,33 @@ module "EKS_rttbl" {
   Purpose = var.Purpose
   }
 
-#   module "EKS_SCGP" {
-#   source      = "../modules/securitygrp"
-#   vpc_id      = "${module.EKS_vpc.vpc_id}"
-#   ingress_rules    = var.ingress_rules
-#   NameSG  = var.NameSG
-#   Owner  = var.Owner
-#   Purpose  = var.Purpose
-#   protocal = var.protocal
-#   secgrp_id   = "${module.EKS_SCGP.secgrp_id}"
-#   }
+  # module "EKS_SCGP" {
+  # source      = "../modules/securitygrp"
+  # vpc_id      = "${module.EKS_vpc.vpc_id}"
+  # ingress_rules    = var.ingress_rules
+  # NameSG  = var.NameSG
+  # Owner  = var.Owner
+  # Purpose  = var.Purpose
+  # protocal = var.protocal
+  # secgrp_id   = "${module.EKS_SCGP.secgrp_id}"
+  # }
 
-#   module "EKS_subnets" {
-#   source      = "../modules/subnets" 
-#   vpc_id    =  "${module.EKS_vpc.vpc_id}"
-#   subnets = var.subnets
-#   subnettag  = var.subnettag
-#   Owner  = var.Owner
-#   Purpose  = var.Purpose
-#   }
-#    module "EKS_Pvtsubnets" {
-#   source      = "../modules/pvtsubnets" 
-#   vpc_id    =  "${module.EKS_vpc.vpc_id}"
-#   pvtsubnets = var.pvtsubnets
-#   pvtsubnettag  = var.pvtsubnettag
-#   Owner  = var.Owner
-#   Purpose  = var.Purpose
-#   }
+  # module "EKS_subnets" {
+  # source      = "../modules/subnets" 
+  # vpc_id    =  "${module.EKS_vpc.vpc_id}"
+  # subnets = var.subnets
+  # subnettag  = var.subnettag
+  # Owner  = var.Owner
+  # Purpose  = var.Purpose
+  # }
+  #  module "EKS_Pvtsubnets" {
+  # source      = "../modules/pvtsubnets" 
+  # vpc_id    =  "${module.EKS_vpc.vpc_id}"
+  # pvtsubnets = var.pvtsubnets
+  # pvtsubnettag  = var.pvtsubnettag
+  # Owner  = var.Owner
+  # Purpose  = var.Purpose
+  # }
 
 module "EKS_routeassociation" {
   source      = "git::https://github.com/radhikaepiq/EKS_PROJECT.git//modules/routeassociation"
