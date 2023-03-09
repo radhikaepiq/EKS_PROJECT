5fb76283c24b370c2d781f7dfcf46606e2ae10d4
radhikaN <radhika@gmail.com>
2023-03-09 15:30:17 +0530

    new

---
+++ modules/internetgw/var.tf
@@ -0,0 +1,21 @@
+
+variable "vpc_id" { type = string }
+variable "NameIG" { type = string }
+variable "Owner" {
+  type = string
+}
+
+variable "Purpose" {
+  type = string
+}
+
+variable "vpc_id" { type = string }
+variable "NameIG" { type = string }
+variable "Owner" {
+  type = string
+}
+
+variable "Purpose" {
+  type = string
+}
+
