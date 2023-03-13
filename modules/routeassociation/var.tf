dc78fdeb18733c9aad76da0a8804d5d636b267b6
sayalimore1 <sayalim9@gmail.com>
2023-03-13 12:22:23 +0530

    2 changes for RAsso by SM

--- modules/routeassociation/var.tf
+++ modules/routeassociation/var.tf
@@ -5,22 +5,6 @@ variable "route_table_id" {
 variable "subnet_id" {
     type = map
 }
-variable "Name" {
-    type = string
-#  default = "EKS_RTtblassociation"
-}
-variable "Owner" {
-  type = string
-  # description = "Value used for tagging Owner"
-  # default = "RadhikaN"
-}
-
-variable "Purpose" {
-  type = string
-  # description = "Purpose of the infrastructure creation"
-  # default = "EKSProject"
-}
-
 
 variable "subnets" {
    type = map
