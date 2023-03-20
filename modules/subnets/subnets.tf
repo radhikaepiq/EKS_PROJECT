56cd5e45bb71979687b1591d21110289f31077f6
Tejas Acharekar <103490327+tejasacharekar1@users.noreply.github.com>
2023-03-20 13:04:43 +0530

    Update in AZ, Commented for automation
    Signed-off-by: Tejas Acharekar <103490327+tejasacharekar1@users.noreply.github.com>

--- modules/subnets/subnets.tf
+++ modules/subnets/subnets.tf
@@ -3,7 +3,7 @@
 resource "aws_subnet" "EKS_subnets" {
   for_each = var.subnets
  
-  availability_zone_id = each.value["az1"]
+#   availability_zone_id = each.value["az1"]
   cidr_block = each.value["cidr"]
   map_public_ip_on_launch = true
   vpc_id = "${var.vpc_id}"
